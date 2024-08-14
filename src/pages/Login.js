40bbd644f1ef0a4bc684c315751891688a2d0d23
MehmetCivril <162918114+MehmetCivril@users.noreply.github.com>
2024-08-14 11:41:42 +0400

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,56 @@
+import React, { useState } from "react"
 import { Button, TextField } from "@mui/material"
-import React from "react"
+import axios from "axios"
 
 function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  console.log(username)
+
+  const handleLogin = async () => {
+    try {
+      //key (sol) backend value (sağ) frontu temsil ediyor
+      let requestObj = {
+        username: username,
+        password: password,
+      }
+      //backend bizden post istediği için
+      let response = await axios.post(
+        "http://localhost:9000/user/login",
+        requestObj
+      )
+      console.log(response.data)
+    } catch (error) {
+      console.log("Login Error", error)
+    }
+  }
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div>
         <div>
-          <TextField variant="standard" label="Username" />
+          <TextField
+            variant="standard"
+            label="Username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className="my-4">
+          <TextField
+            variant="standard"
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
-          <TextField variant="standard" label="Password" />
+          <Button variant="outlined" onClick={handleLogin}>
+            Login
+          </Button>
         </div>
-        <Button variant="outlined">Login</Button>
       </div>
     </div>
   )
